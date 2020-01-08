b21ef38b1030a75aee91fbd95ecd5a3b4098b3d3
vovabndr <vovabndr6661@icloud.com>
2020-01-08 19:49:07 +0200

    Initial Commit

---
+++ Unik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unik
+//
+//  Created by Volodymyr Bondar on 08.01.2020.
+//  Copyright © 2020 Volodymyr Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
